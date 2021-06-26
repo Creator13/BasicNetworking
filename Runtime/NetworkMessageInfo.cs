baa3570b55fe407f8a310692711b9fecc33406d6
Casper van Battum <cvanbattum@gmail.com>
2021-06-26 23:34:31 +0200

    Initial commit

---
+++ Runtime/NetworkMessageInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Networking
+{
+    public static class NetworkMessageInfo
+    {
+        public static readonly Dictionary<ushort, Type> typeMap =
+            new Dictionary<ushort, Type> {
+                {(ushort) BuiltinMessageTypes.Ping, typeof(PingMessage)},
+                {(ushort) BuiltinMessageTypes.Pong, typeof(PongMessage)}
+            };
+    }
+}
